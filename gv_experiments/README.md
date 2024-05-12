f332854f0005e6a3f2c08238a294a22b698616af
ergesmema <77251761+ergesmema@users.noreply.github.com>
2024-05-12 18:41:03 +0200

    fixed structure

--- gv_experiments/README.md
+++ gv_experiments/README.md
@@ -10,5 +10,37 @@ Specifically:
 - `train_recommender_ResAMR.py` is the script used to train the model for recommendation purposes.
 - `train_SplitPCA_LR_classifier.py` and `train_JointPCA_LR_classifier.py` contain the training of the PCA+LR baseline. They respectively apply the PCA step before and after concatenation.
 - `train_1hotSpecies_ResAMR_classifier.py` is the ablation experiment where the MALDI-TOF spectrum is substituted by the 1-hot encoding of the pathogen species.
-- `pretrain_ResAMR_baseline_comparison.py` and `finetune_ResAMR_baseline_comparison.py` are used for the comparison with the single drug-single species from the Weis et al. paper. 
+- `pretrain_ResAMR_baseline_comparison.py` and `finetune_ResAMR_baseline_comparison.py` are used for the comparison with the single drug-single species from the Weis et al. paper.
+
+### May 2024 - XAIML
+To run the training script:
+- First change the DATA_PATH with the absolute value of the directory where you have the driam datasets.
+- For a clean run, make sure you have deleted anything under `/output` and `/binned_data`.
+- Command to run the training (once you are in gv_experiments):
+
+```bash
+python3 training_scripts_may_24/train_ResAMR_classifier_may_24.py \
+--experiment_name "myExperiment1" \
+--experiment_group "ResMLP" \
+--seed 0 \
+--split_type "random" \
+--driams_long_table "../processed_data/DRIAMS_combined_long_table.csv" \
+--drugs_df "../processed_data/drug_fingerprints.csv" \
+--spectra_matrix "../data/spectra_binned_6000_all.npy" \
+--n_epochs 2 \
+--learning_rate 0.0003 \
+--drug_emb_type "fingerprint" \
+--fingerprint_class "morgan_1024" \
+--fingerprint_size 1024 \
+--patience 50 \
+--batch_size 128
+```
+
+
+To run inference:
+- Define the paths at the top of `inference.py`. For now, paths are static for testing purposes.
+- Command to run inference (once you are in gv_experiments):
+`python3 inference_may_24/inference.py`
+
+
 
--- gv_experiments/inference_may_24/inference.py
+++ gv_experiments/inference_may_24/inference.py
@@ -1,18 +1,23 @@
 import sys
+# Assuming the following imports are correctly pointing to your project structure
+sys.path.insert(0, "..")
+sys.path.insert(0, "../../data_split")
 import torch
-from pytorch_lightning import Trainer
-from argparse import Namespace
 import json
 import pandas as pd
 from torch.utils.data import DataLoader
 import numpy as np
 
-# Assuming the following imports are correctly pointing to your project structure
-sys.path.insert(0, "..")
-sys.path.insert(0, "../../data_split")
-from multimodal_amr_tests_erges.models.classifier import Residual_AMR_Classifier
-from multimodal_amr_tests_erges.models.data_loaders import DrugResistanceDataset_Fingerprints, DrugResistanceDataset_Embeddings
-from multimodal_amr_tests_erges.experiments.pl_experiment import Classifier_Experiment
+from multimodal_amr.may_24.models.classifier import Residual_AMR_Classifier
+from multimodal_amr.may_24.models.data_loaders import DrugResistanceDataset_Fingerprints, DrugResistanceDataset_Embeddings
+from multimodal_amr.may_24.experiments.pl_experiment import Classifier_Experiment
+
+config_file = "/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/gv_experiments/outputs/ResMLP/myExperiment1_DRIAMS-['A', 'B', 'C', 'D']_random/config.json"
+checkpoint_file = "/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/gv_experiments/outputs/ResMLP/myExperiment1_DRIAMS-['A', 'B', 'C', 'D']_random/0/lightning_logs/version_0/checkpoints/epoch=1-step=2418.ckpt"
+sample_file = "/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/data/DRIAMS-B/binned_6000/2018/dd7aec30-f6d9-4ebe-91e7-b8399332fc34.txt"
+test_file = "/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/gv_experiments/outputs/ResMLP/myExperiment1_DRIAMS-['A', 'B', 'C', 'D']_random/0/test_set_stratified_species.csv"
+drugs_file = '/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/processed_data/drug_fingerprints.csv'
+
 
 def load_model_from_checkpoint(experiment, checkpoint_path):
     checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
@@ -40,21 +45,15 @@ def inference(experiment, data_loader):
             predictions.extend(predicted_classes.cpu().numpy()) 
     return predictions
 
-config_file = "/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/tests_erges/outputs/ResMLP/myExperiment1_DRIAMS-['A', 'B', 'C', 'D']_stratify_species/config.json"
-checkpoint_file = "/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/tests_erges/outputs/ResMLP/myExperiment1_DRIAMS-['A', 'B', 'C', 'D']_stratify_species/0/lightning_logs/version_0/checkpoints/epoch=1-step=2770.ckpt"
-sample_file = "/Users/em/Desktop/Uni-Spring24/XAIML/MultimodalAMR/DRIAMS-B/binned_6000/2018/ca568529-351a-43af-8cec-7175488f66ea.txt"
-
-
 if __name__ == '__main__':
     with open(config_file, 'r') as file:
         config = json.load(file)
 
 
     sample_data = pd.read_csv(sample_file, sep=" ", index_col=0)
-    test_df = pd.read_csv('/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/tests_erges/data/test_set_stratified_species.csv')
-    drugs_df = pd.read_csv('/Users/em/Desktop/Uni-Spring24/XAIML-gitlab/b4-interpretable-antimicrobial-recommendation/MultimodalAMR/processed_data/drug_fingerprints.csv', index_col=0)
+    test_df = pd.read_csv(test_file)
+    drugs_df = pd.read_csv(drugs_file, index_col=0)
     samples_list = sorted(test_df['sample_id'].unique())  # Ensure this matches your sample indexing strategy
-
 
     spectra_matrix = [sample_data["binned_intensity"].values]
     spectra_matrix = np.vstack(spectra_matrix)
